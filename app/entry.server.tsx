68800f1b1cc72b6b7056fcce92b72bbbddef89ff
doctorbilardo <g.derbes@paisanos.io>
2024-03-08 13:48:50 -0300

    chore: proof lint formatter

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -9,7 +9,7 @@ import type {AppLoadContext, EntryContext} from "@remix-run/node";
 
 import {PassThrough} from "node:stream";
 
-import {createReadableStreamFromReadable} from "@remix-run/node";
+import {createReadableStreamFromReadable} from "     @remix-run/node";
 import {RemixServer} from "@remix-run/react";
 import {isbot} from "isbot";
 import {renderToPipeableStream} from "react-dom/server";
