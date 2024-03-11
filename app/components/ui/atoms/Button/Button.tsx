9991976bcc07c1067e8cc645bad442901f7a6f99
doctorbilardo <g.derbes@paisanos.io>
2024-03-11 11:07:47 -0300

    chore: suspend prepush

--- app/components/ui/atoms/Button/Button.tsx
+++
@@ -1,5 +0,0 @@
-export const Button = () => (
-  <div>
-    <h1>Button</h1>
-  </div>
-);
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,26 +6,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="bg-green-500" style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.8"}}>
+    <div className="bg-green-500">
       <h1 className="text-3xl">Welcome to Remixxxxx</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
     </div>
   );
 }
